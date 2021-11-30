cd085b085bb75c8929e5b05494ce61b7e59d4c07
sarra <saadaniisarah65@gmail.com>
2021-11-30 18:56:52 +0100

    premiere version

---
+++ src/app/pipe.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'pipe'
+})
+export class PipePipe implements PipeTransform {
+
+  transform(c:string): string {
+    return "**"+c+"**";
+  }
+
+}
